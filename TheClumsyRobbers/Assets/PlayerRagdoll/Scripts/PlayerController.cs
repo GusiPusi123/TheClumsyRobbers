4bd12270ac3cf1faac5cebf90a892998402d217f
GusiPusi123 <matveykulmanov0809.1219@gmail.com>
2026-06-12 13:21:14 +0300

    Update 6

--- TheClumsyRobbers/Assets/PlayerRagdoll/Scripts/PlayerController.cs
+++ TheClumsyRobbers/Assets/PlayerRagdoll/Scripts/PlayerController.cs
@@ -35,3 +35,42 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
+
+
+// using UnityEngine;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     public Rigidbody mainRigidbody; // Основная часть, которой управляем
+//     public float moveSpeed = 5f;
+//     public float jumpForce = 7f;
+//     public LayerMask groundLayer;
+//     public Transform groundCheck;
+//     public float groundCheckRadius = 0.2f;
+
+//     private bool isGrounded;
+
+//     void FixedUpdate()
+//     {
+//         // Проверка на землю
+//         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+//         // Ввод движения
+//         float moveX = Input.GetAxis("Horizontal");
+//         float moveZ = Input.GetAxis("Vertical");
+//         Vector3 moveDir = new Vector3(moveX, 0, moveZ).normalized;
+
+//         // Передача движения основной части
+//         Vector3 targetVelocity = moveDir * moveSpeed;
+//         Vector3 currentVelocity = mainRigidbody.velocity;
+
+//         // Обновляем горизонтальные компоненты скорости
+//         mainRigidbody.velocity = new Vector3(targetVelocity.x, currentVelocity.y, targetVelocity.z);
+
+//         // Прыжок
+//         if (Input.GetButtonDown("Jump") && isGrounded)
+//         {
+//             mainRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+//         }
+//     }
+// }
