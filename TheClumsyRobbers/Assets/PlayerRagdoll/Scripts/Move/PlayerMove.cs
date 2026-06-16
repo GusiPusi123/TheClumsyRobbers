7a002051a1db7f8da6698b15d22c75a4b70458c6
GusiPusi123 <matveykulmanov0809.1219@gmail.com>
2026-06-16 22:48:20 +0300

    Update 9

--- TheClumsyRobbers/Assets/PlayerRagdoll/Scripts/Move/PlayerMove.cs
+++ TheClumsyRobbers/Assets/PlayerRagdoll/Scripts/Move/PlayerMove.cs
@@ -1,91 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerMove : MonoBehaviour
+// {
+//     public float Speed;
+//     public float strafeSpeed;
+//     public float JumpForce;
+
+//     public Rigidbody Hips;
+//     public bool isGrounded;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         Hips = GetComponent<RigidBody>();    
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
+
+
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] Rigidbody rigidbody3D;
-    // Если вам нужно вращать объект, проще использовать Transform
-    // [SerializeField] CharacterJoint mainJoint;
-    Vector2 moveInputVector = Vector2.zero;
-    bool JumpButtonPressed = false;
-    float maxspeed = 3f;
-    bool isGrounded = false;
-    RaycastHit[] raycastHits = new RaycastHit[10];
-
-    void Start()
-    {
-        // Инициализация, если нужно
-    }
-
-    void Update()
-    {
-        moveInputVector.x = Input.GetAxis("Horizontal");
-        moveInputVector.y = Input.GetAxis("Vertical");
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            JumpButtonPressed = true;
-        }
-    }
+    public Rigidbody rb; // Rigidbody персонажа
+    public float moveForce = 50f; // сила для передвижения
+    public float maxSpeed = 5f; // максимальная скорость
 
     void FixedUpdate()
     {
-        // Проверка на землю
-        isGrounded = false;
-        int numberOfHits = Physics.SphereCastNonAlloc(
-            rigidbody3D.position,
-            0.1f,
-            Vector3.down,
-            raycastHits,
-            0.5f
-        );
+        // Получение ввода с клавиатуры
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D или стрелки влево/вправо
+        float moveVertical = Input.GetAxis("Vertical"); // W/S или стрелки вверх/вниз
 
-        for (int i = 0; i < numberOfHits; i++)
+        // Создаем вектор направления
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Ограничение скорости
+        if (rb.velocity.magnitude < maxSpeed)
         {
-            if (raycastHits[i].transform.root == transform)
-                continue;
-            isGrounded = true;
-            break;
-        }
-
-        if (!isGrounded)
-        {
-            rigidbody3D.AddForce(Vector3.down * 10f);
-        }
-
-        float inputMagnitude = moveInputVector.magnitude;
-
-        if (inputMagnitude > 0)
-        {
-            // Создаем направление из входных данных
-            Vector3 inputDirection = new Vector3(moveInputVector.x, 0, moveInputVector.y);
-            // Нормализуем, чтобы движение было равномерным
-            Vector3 moveDir = inputDirection.normalized;
-
-            // Применяем силу в направлении движения
-            rigidbody3D.AddForce(moveDir * 10f);
-
-            // Ограничиваем максимальную скорость
-            if (rigidbody3D.velocity.magnitude > maxspeed)
-            {
-                rigidbody3D.velocity = rigidbody3D.velocity.normalized * maxspeed;
-            }
-        }
-
-        // Вращение объекта в сторону движения
-        if (inputMagnitude > 0)
-        {
-            Vector3 inputDirection = new Vector3(moveInputVector.x, 0, moveInputVector.y);
-            Quaternion desiredRotation = Quaternion.LookRotation(inputDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, Time.fixedDeltaTime * 300);
-        }
-
-        // Прыжок
-        if (isGrounded && JumpButtonPressed)
-        {
-            rigidbody3D.AddForce(transform.up * 20f, ForceMode.Impulse);
-            JumpButtonPressed = false;
+            rb.AddForce(movement * moveForce);
         }
     }
 }
